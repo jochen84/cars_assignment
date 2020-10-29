e4a81e29d6099c4233915fe173c73d4d3ccf34e4
jochen84 <simman@rocketmail.com>
2020-10-29 15:49:02 +0100

    CarUserService

---
+++ src/main/java/com/example/cars/services/CarService.java
@@ -0,0 +1,31 @@
+package com.example.cars.services;
+
+import com.example.cars.entities.Car;
+import com.example.cars.repositories.CarRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CarService {
+
+    private final CarRepository carRepository;
+
+    public List<Car> findAll(){
+        return carRepository.findAll();
+    }
+
+    public Car findById(String id){
+        return carRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Could not find a car with that id"));
+    }
+
+    public Car save()
+
+}
