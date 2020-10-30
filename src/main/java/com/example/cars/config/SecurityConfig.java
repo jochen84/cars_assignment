96b31e9104b15f5ee03b6c5c938a2cdb631e0a86
jochen84 <simman@rocketmail.com>
2020-10-30 11:30:55 +0100

    AuthenticationEntryPoint Basic security setup

--- src/main/java/com/example/cars/config/SecurityConfig.java
+++ src/main/java/com/example/cars/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.example.cars.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,8 +16,31 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 //@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AuthenticationEntryPoint entryPoint;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin().disable()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/open/**").permitAll()
+                .antMatchers("/api/**").permitAll()
+                .and()
+                .httpBasic().authenticationEntryPoint(entryPoint)
+                .and()
+                .logout(l -> l.logoutSuccessUrl("/"));
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 }
+
+
