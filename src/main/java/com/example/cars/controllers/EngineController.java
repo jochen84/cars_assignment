d0dc1eea91081a1ed9858fca2349769304051a3c
jochen84 <simman@rocketmail.com>
2020-11-02 13:36:35 +0100

    Removed user access from engine

--- src/main/java/com/example/cars/controllers/EngineController.java
+++ src/main/java/com/example/cars/controllers/EngineController.java
@@ -21,13 +21,13 @@ public class EngineController {
     @Autowired
     private EngineService engineService;
 
-    @Secured({"ROLE_ADMIN", "ROLE_CARDEALER", "ROLE_USER"})
+    @Secured({"ROLE_ADMIN", "ROLE_CARDEALER"})
     @GetMapping
     public ResponseEntity<List<Engine>> findAllEngine(@RequestParam(required = false) String fuel, @RequestParam(required = false) String isSupercharged, @RequestParam(required = false)String enginePosition, @RequestParam(required = false)String cylinders, @RequestParam(required = false)boolean sortByFuel, @RequestParam(required = false)boolean sortByIsSupercharged, @RequestParam(required = false)boolean sortByEnginePosition, @RequestParam(required = false)boolean sortByCylinders){
         return ResponseEntity.ok(engineService.findAll(fuel, isSupercharged, enginePosition, cylinders, sortByFuel, sortByIsSupercharged, sortByEnginePosition, sortByCylinders));
     }
 
-    @Secured({"ROLE_ADMIN", "ROLE_CARDEALER", "ROLE_USER"})
+    @Secured({"ROLE_ADMIN", "ROLE_CARDEALER"})
     @GetMapping("/{id}")
     public ResponseEntity<Engine> findById(@PathVariable String id){
         return ResponseEntity.ok(engineService.findById(id));
